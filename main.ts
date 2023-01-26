22dd093396d71f7f5e0375dcfe25aa728340df24
Pixel9113 <122548027+Pixel9113@users.noreply.github.com>
2023-01-26 10:22:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    value += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    value = 0
+})
+input.onButtonPressed(Button.B, function () {
+    value += -1
+})
+let value = 0
+value = 0
