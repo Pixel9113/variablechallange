13bb0005dddcb376ba80cc32d2798ab2531974c8
Pixel9113 <122548027+Pixel9113@users.noreply.github.com>
2023-01-26 11:10:04 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     value += 1
-    basic.showNumber(value)
+    basic.showNumber(value,25)
 })
 input.onButtonPressed(Button.AB, function () {
     value = 0
-    basic.showNumber(value)
+    basic.showNumber(value,25)
 })
 input.onButtonPressed(Button.B, function () {
     value += -1
-    basic.showNumber(value)
+    basic.showNumber(value,25)
 })
 let value = 0
 value = 0
-basic.showNumber(value)
+basic.showNumber(value,25)
