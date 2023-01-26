71453edd81364af953a1f11e5219afb0c23cf60e
Pixel9113 <122548027+Pixel9113@users.noreply.github.com>
2023-01-26 11:06:54 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     value += 1
+    basic.showNumber(value)
 })
 input.onButtonPressed(Button.AB, function () {
     value = 0
+    basic.showNumber(value)
 })
 input.onButtonPressed(Button.B, function () {
     value += -1
+    basic.showNumber(value)
 })
 let value = 0
 value = 0
+basic.showNumber(value)
